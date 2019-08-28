de0b710b8e51b91908f36d4775867679ea5c6522
KUB4W16 <53148406+KUB4W16@users.noreply.github.com>
2019-08-28 10:53:41 +0200

    Source of pyfetch

--- pyfetch.py
+++ pyfetch.py
@@ -2,25 +2,94 @@
 
 import platform
 import pip
+import os
 import sys
-from termcolor import colored
+import requests
+import pkgutil
+from bs4 import BeautifulSoup
+from colorama import Fore, Back, Style
 
-ver = platform.python_version()
-architecture = platform.architecture()
-pip = pip.__version__
-packages = sys.modules.__len__()
 
-print(
-colored("           ,.:;!!!;:.,", "blue"), colored("                        Python:            ", "blue"), colored(ver, "yellow"), "\n",
-colored("         !!!()::::!!!", "blue"), colored("                        Pip:               ", "blue"), colored(pip, "yellow"), "\n",
-colored("         !:::::;;;;::", "blue"), colored("                        Pip packages:      ", "blue"), colored(packages, "yellow"), "\n",
-colored("   ,,,,,.......!!!!!!", "blue"), colored("                        Operating System:  ", "blue"), colored(architecture[1], "yellow"), "\n",
-colored("  !!!::::::;;;;;;;;;;", "blue"), colored("!!;;::::.,", "yellow"),  colored("             Arhitecture:       ", "blue"), colored(architecture[0], "yellow"), "\n",
-colored(" !!!::::::::;;;;:::::", "blue"), colored("!!;;:::::::\n", "yellow"),
-colored("!!!:::::;;;:''''''''", "blue"), colored(".;;;;;:;;;;;\n", "yellow"),
-colored("!!!::;::::", "blue"),  colored(",.::::;;;:::::::;;;;;;\n", "yellow"),
-colored(" !!!!:::::", "blue"),  colored("::::::::::::::::::;;;\n", "yellow"),
-colored("  !!!:::::", "blue"),  colored(";;;;;;;`````````````\n", "yellow"),
-colored("           !::::::::;!!\n", "yellow"),
-colored("           !::::::()!!!\n", "yellow"),
-colored("            `':;!!!;:'`\n", "yellow"))
+def main():
+    os.system("cls")
+    ver = platform.python_version()
+    architecture = platform.architecture()
+    system = platform.platform()
+    processor = platform.processor()
+    implementation = platform.python_implementation()
+    pip_ver = pip.__version__
+    packages_num = sys.modules.__len__()
+
+
+    python_art = [
+        Fore.BLUE + "          ,.:;!!!;:.,                " + Fore.BLUE + "Python:                " + Fore.YELLOW + ver,
+        Fore.BLUE + "         !!!O:::::!!!                " + Fore.BLUE + "Python implementation: " + Fore.YELLOW + implementation,
+        Fore.BLUE + "         !:::::;;;;::                " + Fore.BLUE + "Pip:                   " + Fore.YELLOW + pip_ver,
+        Fore.BLUE + "   ,,,,,.......!!!!!!" + Fore.YELLOW + " !!;;::.,       " + Fore.BLUE + "Pip packages:          " + Fore.YELLOW + str(packages_num),
+        Fore.BLUE + "  !!!::::::;;;;;;;;;;" + Fore.YELLOW + " !!;;::::.,     ",
+        Fore.BLUE + " !!!::::::::;;;;:::::" + Fore.YELLOW + " !!;;:::::::    " + Fore.BLUE + "Operating System:      " + Fore.YELLOW + system,
+        Fore.BLUE + "!!!:::::;;;:'''''''' " + Fore.YELLOW + ".;;;;;:;;;;;    " + Fore.BLUE + "Architecture:          " + Fore.YELLOW + architecture[0],
+        Fore.BLUE + "!!!::;::::" + Fore.YELLOW + "  ,.::::;;;:::::::;;;;;;   " + Fore.BLUE + "Processor:             " + Fore.YELLOW + processor,
+        Fore.BLUE + " !!!!:::::" + Fore.YELLOW + "  ::::::::::::::::::;;;    ",
+        Fore.BLUE + "  !!!:::::" + Fore.YELLOW + "  ;;;;;;;`````````````     ",
+        Fore.YELLOW + "            !::::::::;!!             ",
+        Fore.YELLOW + "            !::::::;O!!!             ",
+        Fore.YELLOW + "            `':;!!!;:'`              "
+    ]
+
+
+    for i in python_art:
+        print(i)
+
+    print(Fore.WHITE, "\n[1]Show packages list    [2]Find module description    [3]Exit")
+    select = int(input("Enter option number: ")) 
+    if select == 1:
+        print_packages()
+    elif select == 2:
+        pypi_scrapping()
+    elif select == 3:
+        exit()
+    else: 
+        os.system("pause")
+        os.system("cls")
+        main()
+
+
+def print_packages():
+    for i, value in enumerate(pkgutil.iter_modules()):
+        print(Fore.YELLOW, "[", i + 1, "]", Fore.WHITE, value.name)
+    os.system("pause")
+    os.system("cls")
+    main()
+
+
+def pypi_scrapping():
+    name = str(input("Enter module name: "))
+    try:
+        response = requests.get("https://pypi.org/project/{}".format(name))
+
+        if response.status_code == 200:
+            html = BeautifulSoup(response.text, 'html.parser')
+            output = html.find(class_="project-description")
+            print("\n\n", Fore.BLUE, name, Fore.YELLOW, "PyPi Description")
+            print(Fore.WHITE, output.text)
+            os.system("pause")
+            os.system("cls")
+            main()
+        elif response.status_code == 404:
+            print(Fore.RED, "Error 404: No module named %s in PyPi. Please check spelling." % name)
+            os.system("pause")
+            os.system("cls")
+            main()
+        else:
+            print(Fore.RED, "Unknown Error: Something went wrong.")
+            os.system("pause")
+            os.system("cls")
+            main()
+
+
+    except requests.exceptions.ConnectionError:
+        print("Error 404: no internet connection.")
+
+if __name__ == "__main__":
+    main()
