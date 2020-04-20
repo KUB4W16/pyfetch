e2d7c000841249040402797afebc90679ff63c22
KUB4W16 <53148406+KUB4W16@users.noreply.github.com>
2020-04-20 09:05:33 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -16,8 +16,8 @@ function home() {
     description.innerHTML = "<p>pyfetch - information tool like linux screenfetch</p>";
 }
 function about() {
-    description.innerHTML = '<p>pyfetch is an information tool such as <a class="link" href="https://github.com/KittyKatt/screenFetch">screenfetch</a> that we have on linux. It give you the python version, pip version, pip packages number, OS architecture. After runing the script you can choose one of three options: [1]Show packages list, [2]Find module description and [3]Exit. You can get a description of the modules from PyPi, a list of the packages and of course you can exit.</p>';
+    description.innerHTML = '<p>pyfetch is information tool like <a class="link" href="https://github.com/KittyKatt/screenFetch">screenfetch</a> we can see on linux. If you run this script, remember to run script in cmd not in python shell. It give you python version, pip version, pip packages number, OS and architecture. If you run script you can choose one of three options: [1]Show packages list, [2]Find module description and [3]Exit. You can get description of modules from PyPi, list of packages and of course you can exit.</p>';
 }
 function openGithub() {
-    location.href = "https://github.com/KUB4W16/pyfetch";
+    window.open("https://github.com/KUB4W16/pyfetch", "_blank");
 }
